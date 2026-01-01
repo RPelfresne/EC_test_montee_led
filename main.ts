f35c4061028e2d34e92812a0d42d441b9d43243c
RPelfresne <49885062+RPelfresne@users.noreply.github.com>
2026-01-01 16:27:47 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 basic.showIcon(IconNames.Yes)
-let nb_leds = 10
-let montee_1 = 5
+let nb_leds = 130
+let montee_1 = 60
 let dureeMontee = 10000
 let strip = neopixel.create(DigitalPin.P1, nb_leds, NeoPixelMode.RGB)
-let descente_1 = 3
-let montee_2 = 9
+let descente_1 = 10
+let montee_2 = 81
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
 strip.show()
 basic.showIcon(IconNames.Heart)
@@ -12,14 +12,14 @@ basic.pause(100)
 for (let index = 0; index < montee_1; index++) {
     strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
     strip.show()
-    basic.pause(1000)
+    basic.pause(randint(500, 1000))
     strip.shift(1)
 }
 strip.shift(-1)
 basic.pause(2000)
 for (let index = 0; index < descente_1; index++) {
-    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Black))
-    basic.pause(2000)
+    strip.setPixelColor(59, neopixel.colors(NeoPixelColors.Black))
+    basic.pause(randint(500, 2000))
     strip.show()
     strip.shift(-1)
 }
@@ -28,7 +28,7 @@ basic.pause(2000)
 for (let index = 0; index < montee_2; index++) {
     strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
     strip.show()
-    basic.pause(2000)
+    basic.pause(randint(500, 1000))
     strip.shift(1)
 }
 basic.forever(function () {
